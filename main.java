239b6940776bd8820d085cb5427996d443a4dd3d
Kallu17 <56933682+Kallu17@users.noreply.github.com>
2020-03-11 23:36:32 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Circulo{
+    public static void main(String[] args){
+        int raio = 5;
+        double area;
+        
+        area = Math.PI*(raio*raio);
+        
+        System.out.printf("A area de um circulo de %dm de raio é igual a %2fm",raio,area);
+    }
+}
